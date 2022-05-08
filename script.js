3908863b22b36fed39962c917e603f17fd94a12d
Tino Nguyen <101163927+Verreauxx@users.noreply.github.com>
2022-05-07 19:38:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
+    
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
+    
+    
     $('.saveBtn').on('click', function () {
 
       var value = $(this).siblings('.description').val();
@@ -35,7 +48,7 @@ $(document).ready(function () {
 
     var interval = setInterval(hourUpdater, 15000);
   
-    $('#currentDay').text(moment().format('dddd, MMMM Do'));
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
 
     $('#hour-9 .description').val(localStorage.getItem('hour-9'));
     $('#hour-10 .description').val(localStorage.getItem('hour-10'));
